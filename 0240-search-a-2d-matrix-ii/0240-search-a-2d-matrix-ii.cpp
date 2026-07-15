5ae749ad3c22e8afa99d0e80cf9750c70b7dee8e
hameedkhan-12 <hameed404404@gmail.com>
2026-07-15 18:49:20 +0500

    Time: 109 ms (9.32%), Space: 18.8 MB (38.9%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -13,11 +13,14 @@ public:
         int left = 0;
         int right = row.size() - 1;
 
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(row[mid] == target) return true;
-            else if(row[mid] < target) left = mid + 1;
-            else right = mid - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == target)
+                return true;
+            else if (row[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
     }
