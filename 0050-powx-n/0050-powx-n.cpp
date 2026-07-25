5ececb2b22fcd87df4acf4a8bfdde32f0dd6f452
hameedkhan-12 <hameed404404@gmail.com>
2026-07-25 09:36:45 +0500

    Time: 0 ms (100%), Space: 8.4 MB (99.84%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if(n == 0) return 1;
-        if(n == 1) return x;
-        long long N = n;
-        if(n<0){
-            x = 1/x;
-            N = -N;
-        }
-        return solve(x,N);
-    }
-    double solve(double x, long long n){
-        if(n == 0) return 1;
-        if(n == 1) return x;
+        if (n == 0)
+            return 1;
+        if (n == 1)
+            return x;
 
-        double half = solve(x, n/2);
-        if(n%2==0) return half * half;
+        long long N = n;
+        if (n < 0) {
+            N = -N;
+            x = 1 / x;
+        }
+
+        return solve(x, N);
+    }
+    double solve(double x, long long n) {
+        if (n == 0)
+            return 1;
+
+        double half = solve(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
         return x * half * half;
     }
 };
