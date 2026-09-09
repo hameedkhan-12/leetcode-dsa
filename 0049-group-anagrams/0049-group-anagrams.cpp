b08c4b8ce337f5b36c1a05b9d3d96c6439454084
hameedkhan-12 <hameed404404@gmail.com>
2026-09-10 00:10:13 +0500

    Time: 51 ms (15.44%), Space: 31.1 MB (15.56%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            int freq[26] = {};
+            for (char c : s) {
+                freq[c - 'a']++;
+            }
+            string key;
+            for (int i = 0; i < 26; i++) {
+                key += "#";
+                key += to_string(freq[i]);
+            }
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> result;
+        for (auto& [key, group] : mp) {
+            result.push_back(group);
+        }
+        return result;
+    }
+};
