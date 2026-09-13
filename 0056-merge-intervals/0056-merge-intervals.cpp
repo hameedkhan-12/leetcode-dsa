e3151affc80dfda594e91050d5e99c83a3896c2c
hameedkhan-12 <hameed404404@gmail.com>
2026-09-14 01:51:00 +0500

    Time: 3 ms (89.29%), Space: 23.8 MB (60.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -3,21 +3,20 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty())
             return {};
-        vector<vector<int>> result;
+
         sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> result;
         result.push_back(intervals[0]);
-        int start = intervals[0][0];
-        int end = intervals[0][1];
 
         for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] <= end) {
-                end = max(end, intervals[i][1]);
-                result.back()[1] = end;
-            }else{
+            if (intervals[i][0] <= result.back()[1]) {
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
+            } else {
                 result.push_back(intervals[i]);
-                end = intervals[i][1];
             }
         }
+
         return result;
     }
 };
