cf3b8e507a2b8e382be4bc93ec041478c157248b
hameedkhan-12 <hameed404404@gmail.com>
2026-09-21 10:02:06 +0500

    Time: 68 ms (19.26%), Space: 94.3 MB (12.4%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int atMost(vector<int>& nums, int k) {
-
-        if (k < 0)
-            return 0;
-        int left = 0;
-        int count = 0;
-        int oddCount = 0;
-        for (int right = 0; right < nums.size(); right++) {
-            if (nums[right] % 2 == 1) {
-                oddCount++;
-            }
-            while (oddCount > k) {
-                if (nums[left] % 2 == 1) {
-                    oddCount--;
-                }
-                left++;
-            }
-            count += right - left + 1;
-        }
-
-        return count;
-    }
     int numberOfSubarrays(vector<int>& nums, int k) {
-        return atMost(nums, k) - atMost(nums, k - 1);
+        unordered_map<int,int> mp;
+        int oddCount = 0;
+        mp[oddCount] = 1;
+
+        int count = 0;
+        for(int i=0; i<nums.size(); i++){
+            oddCount += (nums[i] % 2);
+
+            int needed = oddCount - k;
+            if(mp.count(needed)){
+                count += mp[needed];
+            }
+            mp[oddCount]++;
+        }
+        return count;
     }
 };
