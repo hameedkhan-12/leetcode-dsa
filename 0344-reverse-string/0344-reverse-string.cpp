cfdba36b4a728ccea74e5ba9023744f567c5ea0b
hameedkhan-12 <hameed404404@gmail.com>
2026-09-25 11:45:51 +0500

    Time: 0 ms (100%), Space: 27.3 MB (51.83%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -5,9 +5,11 @@ public:
         int right = s.size() - 1;
 
         while(left < right){
-            swap(s[left], s[right]);
-            left++;
-            right--;
+           char temp = s[left];
+           s[left] = s[right];
+           s[right] = temp;
+           left++;
+           right--;
         }
     }
 };
